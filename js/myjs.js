2be4cd49f2abf3d23ec432bd586f3c3935b1268e
Ben Singer <benjamin.singer@helpsystems.com>
2015-02-19 16:15:18 -0600

    switched js widthand height

--- js/myjs.js
+++ js/myjs.js
@@ -4,4 +4,14 @@ var setBkrd = function(){
 	document.body.setAttribute('style', 'background-image: ' + pattern.dataUrl);
 }
 
-$('document').ready(setBkrd);
+$(document).ready(function(){
+	setBkrd();
+
+	var c = $('.container');
+    if (window.devicePixelRatio > 1) {
+        var d =  $(document);
+        c.width(c.width() * 2);
+        c.height(c.height() * 2);
+    }
+    c.offset( {left: ($(document).width() / 2) - (c.width() / 2), top: ($(document).height() / 2) - (c.height() / 2)});
+});
