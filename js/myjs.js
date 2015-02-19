049c555a25d08d2e1fae372a8aefd37ed2f32023
Ben Singer <benjamin.singer@helpsystems.com>
2015-02-19 16:18:51 -0600

    nvm bro

--- js/myjs.js
+++ js/myjs.js
@@ -5,13 +5,5 @@ var setBkrd = function(){
 }
 
 $(document).ready(function(){
-	setBkrd();
-
-	var c = $('.container');
-    if (window.devicePixelRatio > 1) {
-        var d =  $(document);
-        c.width(c.width() * 2);
-        c.height(c.height() * 2);
-    }
-    c.offset( {left: ($(document).width() / 2) - (c.width() / 2), top: ($(document).height() / 2) - (c.height() / 2)});
+	setBkrd();	
 });
