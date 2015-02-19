d647a4ca5f3800d4c0edb1cb57f5bc70089f2b7c
Ben Singer <benjamin.singer@helpsystems.com>
2015-02-19 15:49:48 -0600

    bkrd now as tall as width for phones

--- js/myjs.js
+++ js/myjs.js
@@ -1,6 +1,6 @@
 var setBkrd = function(){
 	var t = new Trianglify();
-	var pattern = t.generate(1920, 1080);
+	var pattern = t.generate(1920, 1920);
 	document.body.setAttribute('style', 'background-image: ' + pattern.dataUrl);
 }
 
