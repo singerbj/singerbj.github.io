39119d043dff2a258261dbe5061ce27aa6ae5acb
Benjamin Singer <benjaminjsinger@gmail.com>
2016-03-26 12:23:02 -0500

    awesome updates

--- js/myjs.js
+++ js/myjs.js
@@ -1,9 +1,9 @@
-var setBkrd = function(){
-	var t = new Trianglify();
-	var pattern = t.generate(1920, 1920);
-	document.body.setAttribute('style', 'background-image: ' + pattern.dataUrl);
-}
-
-$(document).ready(function(){
-	setBkrd();	
-});
+// var setBkrd = function(){
+// 	var t = new Trianglify();
+// 	var pattern = t.generate(1920, 1920);
+// 	document.body.setAttribute('style', 'background-image: ' + pattern.dataUrl);
+// }
+//
+// $(document).ready(function(){
+// 	setBkrd();
+// });
